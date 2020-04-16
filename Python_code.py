407f9e367293bdfc6cd78972225563f36e069e14
Moeshalaby <moshalaby@live.com>
2020-04-15 20:54:08 -0600

    This is for capstone project

--- Python_code.py
+++ Python_code.py
@@ -1,3 +1,8 @@
 git@github.com:Moshalaby1/Findata.git
 Print("Hello World!")
+#This notebook will be mainly used for capstone project
+import pandas as pd
+import numpy as np
+Print("Hello Capstone Project Course")
+
 
